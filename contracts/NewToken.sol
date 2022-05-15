cccc8f3a4991668c853c06a41a0456b41ce7615b
difof <hesyoamaster@protonmail.com>
2022-05-15 23:52:03 +0430

    added license

--- contracts/NewToken.sol
+++ contracts/NewToken.sol
@@ -1,14 +1,11 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/utils/Context.sol';
 
 contract NewToken is Context, ERC20 {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-
-    constructor(uint256 initialSupply) ERC20("NewToken", "NTK") {
+    constructor(uint256 initialSupply) ERC20('NewToken', 'NTK') {
         _mint(msg.sender, initialSupply);
     }
 }
